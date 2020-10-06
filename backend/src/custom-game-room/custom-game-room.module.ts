89c1754c938af8659954a0e81bdfa3d3b969cdf6
Watcharit Tuntayakul <watcharit.new@gmail.com>
2020-10-06 15:58:51 +0700

    Add chat service to provider (fix bug)

--- backend/src/custom-game-room/custom-game-room.module.ts
+++ backend/src/custom-game-room/custom-game-room.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 // import { TypeOrmModule } from '@nestjs/typeorm';
 import { CustomGameRoomGateway } from './custom-game-room.gateway';
 import { CustomGameRoomService } from './custom-game-room.service';
+import { ChatService } from './chat.service';
 import { CustomGameRoomController } from './custom-game-room.controller';
 
 @Module({
   imports: [],
   controllers: [CustomGameRoomController],
-  providers: [CustomGameRoomGateway, CustomGameRoomService],
+  providers: [CustomGameRoomGateway, CustomGameRoomService, ChatService],
   exports: [],
 })
 export class CustomGameRoomModule {}
