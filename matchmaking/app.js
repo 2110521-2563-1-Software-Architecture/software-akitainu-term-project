1dc61ebe78e9a71c1e5f1651c4859649ff7a42e2
Sumet Chorthongdee <sumetchor@gmail.com>
2020-11-18 19:07:45 +0700

    join-custom-room message to frontend

--- matchmaking/app.js
+++ matchmaking/app.js
@@ -39,7 +39,7 @@ io.on("connection", (socket) => {
   socket.on("create-custom-room", (data) => {
     console.log("create custom room");
     console.log(data);
-    room.createCustomRoom(data.userId);
+    room.createCustomRoom(data.userId, socket.id);
     // let userId = socketIdToUserId[socket.id];
     // if (userId) {
     //   room.quitSearchRanked(userId);
